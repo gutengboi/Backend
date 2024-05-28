8dda645a94af445f4acc231b50b3bff45ad77039
gutengboi <animashaunolayinka195@gmail.com>
2024-05-28 15:06:41 +0100

    added mongodb url

--- db/index.js
+++ db/index.js
@@ -1,14 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-console.log('Attempting to connect to MongoDB...');
-mongoose.connect('mongodb://localhost:27017/auth-app', {
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true,
-    // useCreateIndex: true
-})
+console.log("Attempting to connect to MongoDB...");
+mongoose
+  .connect(
+    "mongodb+srv://test_user:test_pwd@cluster0.ay0jjxa.mongodb.net/auth-app",
+    {
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
+      // useCreateIndex: true
+    }
+  )
   .then(() => {
-      console.log('MongoDB connected');
+    console.log("MongoDB connected");
   })
   .catch((err) => {
-      console.error('MongoDB connection error:', err);
+    console.error("MongoDB connection error:", err);
   });
